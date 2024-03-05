11d795681cdfad6db731aecc66329688dc547af7
FilipeGoncalvesB <161300604+FilipeGoncalvesB@users.noreply.github.com>
2024-03-05 14:04:19 +0100

    Update HAL_LCD_I2C_Ctrl.c
    Working version

--- HAL_LCD_I2C_Ctrl.c
+++ HAL_LCD_I2C_Ctrl.c
@@ -14,55 +14,60 @@
 #include "lcdlib/util.h"
 #include "HAL_LCD_I2C_Ctrl.h"
 #include "HAL/HAL_I2C.h"
+#include "driverlib/sysctl.h"
 
 
 void LCDinit(char addr)
 {
-    LCDsendCommand(addr, LCD_4BIT_MODE | LCD_2_LINES | LCD_FONT_5x10);
-    LCDsendCommand(addr, LCD_ENTRY_MODE);
-    LCDdisplayOn(addr);
+    LCDcommand(addr, LCD_4BIT_MODE | LCD_2_LINES | LCD_FONT_5x10);
+    LCDdisplayOff(addr);
     LCDclear(addr);
+    SysCtlDelay(SysCtlClockGet() / (5000 * 3)); // Delai d'attente
+    LCDcommand(addr, LCD_ENTRY_MODE);
+    LCDdisplayOn(addr);
 };
 
 
 void LCDenablePulse(char addr, char data)
 {
     I2CWrite(addr, data | LCD_PIN_E);
-
-    // delay >450ns
-    delay_us(0.5);
+    //delay >450ns, 200us
+    SysCtlDelay(SysCtlClockGet() / (5000 * 3));
 
     I2CWrite(addr, data & ~LCD_PIN_E);
-
-    // delay >450ns
-    delay_us(0.5);
+    //delay >450ns
+    SysCtlDelay(SysCtlClockGet() / (5000 * 3));
 };
 
 
-void LCDsendCommand(char addr, char cmd)
+void LCDcommand(char addr, char cmd)
 {
-    char up_bites = (cmd & 0xF0) & ~LCD_PIN_RS;
-    char low_bites = (cmd << 4) & ~LCD_PIN_RS;
+    char up_bites = (cmd & 0xF0) & ~LCD_PIN_RS | LCD_PIN_P3;
+    char low_bites = (cmd << 4) & ~LCD_PIN_RS | LCD_PIN_P3;
 
+    I2CWrite(addr, up_bites);
     LCDenablePulse(addr, up_bites);
 
+    I2CWrite(addr, low_bites);
     LCDenablePulse(addr, low_bites);
 };
 
 
-void LCDsendData(char addr, char data)
+void LCDdata(char addr, char data)
 {
-    char up_bites = (data & 0xF0) | LCD_PIN_RS;
-    char low_bites = (data << 4) | LCD_PIN_RS;
+    char up_bites = (data & 0xF0) |LCD_PIN_RS | LCD_PIN_P3;
+    char low_bites = (data << 4) |LCD_PIN_RS | LCD_PIN_P3;
 
+    I2CWrite(addr, up_bites);
     LCDenablePulse(addr, up_bites);
 
+    I2CWrite(addr, low_bites);
     LCDenablePulse(addr, low_bites);
 };
 
 
 
-void LCDsetCursorPosition(char addr, char line, char col)
+void LCDsetCursor(char addr, char line, char col)
 {
     char address;
 
@@ -80,76 +85,56 @@ void LCDsetCursorPosition(char addr, char line, char col)
 
     address |= col;
 
-    LCDsendCommand(addr, 0x80 | address);
+    LCDcommand(addr, 0x80 | address);
 };
 
 
 void LCDclear(char addr)
 {
-    LCDsendCommand(addr, LCD_DISP_CLEAR);
+    LCDcommand(addr, LCD_DISP_CLEAR);
     LCDhome(addr);
 };
 
 
 void LCDhome(char addr)
 {
-    LCDsendCommand(addr, LCD_RETURN_HOME);
+    LCDcommand(addr, LCD_RETURN_HOME);
 };
 
 
 void LCDcursorBlink(char addr)
 {
-    LCDsendCommand(addr, LCD_CURSOR_BLINK);
+    LCDcommand(addr, LCD_CURSOR_BLINK);
 };
 
 
 void LCDcursorOff(char addr)
 {
-    LCDsendCommand(addr, LCD_DISP_ON);
+    LCDcommand(addr, LCD_DISP_ON);
 };
 
 
 void LCDcursorOn(char addr)
 {
-    LCDsendCommand(addr, LCD_CURSOR_ON);
+    LCDcommand(addr, LCD_CURSOR_ON);
 };
 
 
 void LCDdisplayOff(char addr)
 {
-    LCDsendCommand(addr, LCD_DISP_OFF);
+    LCDcommand(addr, LCD_DISP_OFF);
 };
 
 
 void LCDdisplayOn(char addr)
 {
-    LCDsendCommand(addr, LCD_DISP_ON);
-};
-
-void LCDbacklightON(char addr)
-{
-    I2CWrite(addr, LCD_PIN_P3);
-};
-
-
-void LCDbacklightOFF(char addr)
-{
-    I2CWrite(addr, 0x00);
-};
-
-
-void LCDprint(char addr, char *text)
-{
-    char *c;
-    c = text;
-
-    while ((c != 0) && (*c != 0))
-        {
-            LCDsendData(addr, *c);
-            c++;
-        }
+    LCDcommand(addr, LCD_DISP_ON);
 };
 
 
 
-
+void LCDprint(char addr, char *text) {
+    while (*text) {
+        LCDdata(addr, *text++);
+    }
+}
